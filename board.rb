f34ed01ba9cf7a2408e893c58a8a75233d75c9cc
mbucataru <mbucatarugithub@gmail.com>
2023-08-25 18:55:55 +0300

    Add basic projec

--- board.rb
+++ board.rb
@@ -1,5 +1,11 @@
 # Defines the grid of squares in the board.
 # May or may not be neccesary
 class Board
+  def initialize
+    @board = build_board
+  end
 
+  def build_board
+
+  end
 end
--- knight.rb
+++ knight.rb
@@ -1,4 +1,6 @@
 # Defines the location, path searching, and moving for the Knight
 class Knight
+  def initialize
 
+  end
 end
